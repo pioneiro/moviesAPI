7329adc330139fa7ad3fa2e2d85ceb979732157a
Soumyajit Das <mepioneiro@gmail.com>
2021-05-19 23:57:06 +0530

    Refactor Code

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const PORT = 5192;
+const PORT = process.env.PORT || 5192;
 
-const express = require("express");
-const api = require("needle");
+import express from "express";
+import needle from "needle";
+import { config } from "dotenv";
+
+config();
 
 const index = express();
 
@@ -9,52 +12,83 @@ index.set("view engine", "ejs");
 
 index.use(express.static("assets"));
 
-index.get("/", function(req, res) {
+index.get("/", (req, res) => {
 	res.render("index");
 });
 
-index.get("/results", function(req, res) {
-	var q = req.query.q,
+index.get("/results", (req, res) => {
+	const q = req.query.q,
 		type = req.query.type,
-		page = req.query.page || 1;	
-	api.get(`http://www.omdbapi.com/?apikey=thewdb&s=${q}&type=${(type== "all")?"":req.query.type}&page=${page}`, function(err, apiRes) {
-		if(!err && apiRes.statusCode == 200) {
-			const data = apiRes.body;
-			if(data.Response == "True") {
-				res.render("results", {data: data, srcv: {q: q, type: type, page: page}});
+		page = req.query.page || 1;
+	needle.get(
+		`http://www.omdbapi.com/?apikey=${process.env.api_key}&s=${q}&type=${
+			type == "all" ? "" : req.query.type
+		}&page=${page}`,
+		(err, apiRes) => {
+			if (!err && apiRes.statusCode == 200) {
+				const data = apiRes.body;
+				if (data.Response == "True") {
+					res.render("results", {
+						data: data,
+						srcv: { q: q, type: type, page: page },
+					});
+				} else {
+					res.render("error", {
+						error:
+							"An error occured while retriving data. Please check your input and try again.",
+					});
+				}
+			} else if (apiRes.statusCode != 200) {
+				res.render("error", {
+					error:
+						"Error Code " +
+						apiRes.statusCode +
+						". An error occured while retriving data.",
+				});
 			} else {
-				res.render("error", {error: "An error occured while retriving data. Please check your input and try again."});
+				res.render("error", {
+					error: "An error occured while retriving data. ERROR Details: " + err,
+				});
 			}
-		} else if(apiRes.statusCode != 200) {
-			res.render("error", {error: "Error Code " + apiRes.statusCode + ". An error occured while retriving data."});
-		} else {
-			res.render("error", {error: "An error occured while retriving data. ERROR Details: " + err});
 		}
-	});
+	);
 });
 
-index.get("/details", function(req, res) {
-	var movieID = req.query.movieID;
-	api.get(`http://www.omdbapi.com/?apikey=thewdb&plot=full&i=${movieID}`, function(err, apiRes) {
-		if(!err && apiRes.statusCode == 200) {
-			const movie = apiRes.body;
-			if(movie.Response == "True") {
-				res.render("details", {movie: movie});
+index.get("/details", (req, res) => {
+	const movieID = req.query.movieID;
+	needle.get(
+		`http://www.omdbapi.com/?apikey=${process.env.api_key}&plot=full&i=${movieID}`,
+		(err, apiRes) => {
+			if (!err && apiRes.statusCode == 200) {
+				const movie = apiRes.body;
+				if (movie.Response == "True") {
+					res.render("details", { movie: movie });
+				} else {
+					res.render("error", {
+						error:
+							"An error occured while retriving data. Please check your input and try again.",
+					});
+				}
+			} else if (apiRes.statusCode != 200) {
+				res.render("error", {
+					error:
+						"Error Code " +
+						apiRes.statusCode +
+						". An error occured while retriving data.",
+				});
 			} else {
-				res.render("error", {error: "An error occured while retriving data. Please check your input and try again."});
+				res.render("error", {
+					error: "An error occured while retriving data. ERROR Details:" + err,
+				});
 			}
-		} else if(apiRes.statusCode != 200) {
-			res.render("error", {error: "Error Code " + apiRes.statusCode + ". An error occured while retriving data."});
-		} else {
-			res.render("error", {error: "An error occured while retriving data. ERROR Details:" + err});
 		}
-	});
+	);
 });
 
-index.get("/*", function(req, res) {
-	res.render("error", {error: "The page requested couldn't be found."});
+index.get("/*", (req, res) => {
+	res.render("error", { error: "The page requested couldn't be found." });
 });
 
-index.listen(process.env.PORT || PORT, process.env.IP, function() {
-	console.log("server initialized at port " + (process.env.PORT || PORT));
+index.listen(PORT, process.env.IP, () => {
+	console.log("server initialized at port " + PORT);
 });
